1246cceb65714df3685e0d9eca1992f93763a48f
Felipe <115735627+FelpzzzEX@users.noreply.github.com>
2024-07-21 20:55:16 +0000

    Função que lê o arquivo e representa numa matriz, separando cada caractere em uma coordenada.

---
+++ def ler_arquivo_como_matriz(caminho_arqu.py
@@ -0,0 +1,16 @@
+def ler_arquivo_como_matriz(caminho_arquivo):
+    with open(caminho_arquivo, 'r') as arquivo:
+        linhas = arquivo.readlines()
+    
+    matriz = [list(linha.strip()) for linha in linhas]
+    return matriz
+
+# Exemplo de uso
+caminho_arquivo = 'maze/toy.txt'
+matriz = ler_arquivo_como_matriz(caminho_arquivo)
+
+# Exibindo a matriz
+for linha in matriz:
+    print(linha)
+    
+print(matriz[1][0])  # Isso deve retornar 'S'
